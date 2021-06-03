8fdf1ebbb000a68ad81e8e15975c375b954248d4
brunomotadev <brunomotadev@gmail.com>
2021-06-03 19:57:31 -0300

    refatorando calculo IMC e cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:cadastro_completo/src/cadastro.dart';
+import 'package:cadastro_completo/src/calc-imc.dart';
 import 'package:cadastro_completo/src/calculo-idade.dart';
 import 'package:cadastro_completo/src/carrinho-compras.dart';
 
 void main(List<String> arguments) {
   if (arguments[0] == "calculo-idade") {
     calculoIdade();
+  } else if (arguments[0] == "imc") {
+    calculoImc();
   } else if (arguments[0] == "carrinho-compras") {
     carrinhoCompras();
+  } else if (arguments[0] == "cadastro") {
+    cadastrarPessoa();
   } else {
     print("=== ESSE PROGRAMA NÃO EXISTE");
   }
