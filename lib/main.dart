1018f307db6dabd23c12dc5ac6c580e2f24de58d
brunomotadev <brunomotadev@gmail.com>
2021-06-03 19:12:03 -0300

    estrutura de decisão com argumentos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
 void main(List<String> arguments) {
-  print(arguments);
+  if(arguments[0] == "hackear"){
+    print("hackeamento concluido");
+  }else if(arguments[0] == "imprimir") {
+    print("executando impressao");
+  }
 }
